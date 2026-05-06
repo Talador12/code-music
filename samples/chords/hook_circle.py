8f7aa7f54b0ff4e1e2cbe27b309d21732ac1f912
Keith Adler <kadler@cloudflare.com>
2026-05-06 14:34:12 -0500

    theory: add hook-first song generator

---
+++ samples/chords/hook_circle.py
@@ -0,0 +1,5 @@
+"""Hook generator: circle harmony plus a repeated melodic cell."""
+
+from code_music.theory import generate_hook_song
+
+song = generate_hook_song("zedd", key="C", bpm=128, bars=8, seed=42)
